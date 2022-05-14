5ddd4e520a5f8ff56c9155985eedb6a839e48acd
Frederik Jensen <frederikhj0@gmail.com>
2022-05-14 19:52:55 +0200

    tweaked som code

--- app/src/main/java/com/example/myfirstapp/views/DiceActivity.kt
+++ app/src/main/java/com/example/myfirstapp/views/DiceActivity.kt
@@ -9,19 +9,26 @@ import com.example.myfirstapp.R
 import com.example.myfirstapp.models.Dice
 
 class DiceActivity : AppCompatActivity() {
+    private lateinit var dice: Dice
+    private lateinit var rollButton: Button
+    private lateinit var rolledNumberTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dice)
 
-        val rollButton = findViewById<Button>(R.id.rollButton)
-        val rolledNumberTextView = findViewById<TextView>(R.id.rolledNumberTextView)
-        val dice = Dice(6)
+        rollButton = findViewById(R.id.rollButton)
+        rolledNumberTextView = findViewById(R.id.rolledNumberTextView)
 
         rollButton.setOnClickListener {
-            val rolledNumber = dice.roll()
-            rolledNumberTextView.text = "$rolledNumber"
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val rolledNumber = dice.roll()
+        rolledNumberTextView.text = "$rolledNumber"
+        val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+        toast.show()
     }
 }
