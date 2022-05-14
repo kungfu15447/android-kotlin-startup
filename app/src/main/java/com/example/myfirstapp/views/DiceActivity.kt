931393a854b4e03d884c5a1f9c8bfc3f1764e33b
Frederik Jensen <frederikhj0@gmail.com>
2022-05-14 19:48:41 +0200

    Can now roll dice

--- app/src/main/java/com/example/myfirstapp/views/DiceActivity.kt
+++ app/src/main/java/com/example/myfirstapp/views/DiceActivity.kt
@@ -2,11 +2,26 @@ package com.example.myfirstapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.example.myfirstapp.R
+import com.example.myfirstapp.models.Dice
 
 class DiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dice)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val rolledNumberTextView = findViewById<TextView>(R.id.rolledNumberTextView)
+        val dice = Dice(6)
+
+        rollButton.setOnClickListener {
+            val rolledNumber = dice.roll()
+            rolledNumberTextView.text = "$rolledNumber"
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
